4b9130bd7fdef4a026cf22e2f6f295b51a982c69
gil zhang <gil.zhang.dev@outlook.com>
2024-10-13 14:19:48 +0800

    Generate an Id

--- src/Plugins/BotSharp.Plugin.LiteDBStorage/LiteDBBase.cs
+++ src/Plugins/BotSharp.Plugin.LiteDBStorage/LiteDBBase.cs
@@ -4,8 +4,28 @@ namespace BotSharp.Plugin.LiteDBStorage;
 
 public abstract class LiteDBBase
 {
-    [BsonId]
-    public string Id { get; set; }
+    private string id;
+
+    public string Id
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                id = GenerateId();
+            }
+            return id;
+        }
+        set { id = value; }
+    }
+
+    private string GenerateId()
+    {
+        // Generate and return the id here
+        // You can use any logic to generate the id
+        // For example, you can use Guid.NewGuid().ToString() to generate a unique id
+        return Guid.NewGuid().ToString();
+    }
 }
 
 
