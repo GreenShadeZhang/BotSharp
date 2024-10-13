3236e828b5b32288031c529d5667d603d91c4910
gil zhang <gil.zhang.dev@outlook.com>
2024-10-13 14:24:34 +0800

    update litedb project config

--- src/Plugins/BotSharp.Plugin.LiteDBStorage/Using.cs
+++ src/Plugins/BotSharp.Plugin.LiteDBStorage/Using.cs
@@ -9,3 +9,5 @@ global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using System.Text;
 global using System.Linq;
+global using System;
+global using System.Collections.Generic;
