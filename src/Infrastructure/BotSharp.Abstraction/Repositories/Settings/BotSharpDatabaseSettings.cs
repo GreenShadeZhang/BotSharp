8b29a476c8c7aa20abddf870df888e5cdef79675
Gil Zhang <gil.zhang.dev@outlook.com>
2025-06-18 23:25:53 +0800

    add ef and postgresql support

--- src/Infrastructure/BotSharp.Abstraction/Repositories/Settings/BotSharpDatabaseSettings.cs
+++ src/Infrastructure/BotSharp.Abstraction/Repositories/Settings/BotSharpDatabaseSettings.cs
@@ -5,6 +5,7 @@ public class BotSharpDatabaseSettings : DatabaseBasicSettings
     public string[] Assemblies { get; set; } = [];
     public string FileRepository { get; set; } = string.Empty;
     public string BotSharpMongoDb { get; set; } = string.Empty;
+    public string BotSharpPostgreSql { get; set; } = string.Empty;
     public string TablePrefix { get; set; } = string.Empty;
     public DbConnectionSetting BotSharp { get; set; } = new();
     public string Redis { get; set; } = string.Empty;
---
+++ src/Plugins/BotSharp.Plugin.EntityFrameworkCore/Repository/EfCoreRepository.Role.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotSharp.Plugin.EntityFrameworkCore.Repository;
+
+public partial class EfCoreRepository
+{
+}
